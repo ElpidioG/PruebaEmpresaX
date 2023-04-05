5bf2be9ca1e589bec32df55acda71f1877d02845
Elpidio Márquez <71982206+ElpidioG@users.noreply.github.com>
2023-04-05 00:27:37 -0400

    Add files via upload

---
+++ emprx.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE EmpresaX;
+Use EmpresaX
+
+CREATE TABLE Cliente (
+    Id INT PRIMARY KEY,
+    Nombre VARCHAR(50),
+    Apellido VARCHAR(50),
+	Telefono Varchar (50)
+);
+
+CREATE TABLE Direccion (
+    Id INT PRIMARY KEY,
+    Calle VARCHAR(50),
+    Ciudad VARCHAR(50),
+    Estado VARCHAR(50),
+    Pais VARCHAR(50),
+    ClienteId INT,
+    FOREIGN KEY (ClienteId) REFERENCES Cliente(Id)
+);
